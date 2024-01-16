29ac4d1f4824037b8471302f49a51e4937995cb5
Andrey Mazunin <97140182+js-easy-school@users.noreply.github.com>
2024-01-16 14:46:54 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,2 +1,9 @@
-var myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+import arrr
+from pyscript import document
+
+
+def translate_english(event):
+    input_text = document.querySelector("#english")
+    english = input_text.value
+    output_div = document.querySelector("#output")
+    output_div.innerText = arrr.translate(english)
